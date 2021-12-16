4c8013c96f64797518df52f9947498f3686ee46b
Gabriel Rocha <gabriel.rocha@conexasaude.com.br>
2021-12-16 19:38:00 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
+import { url } from "./url";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Poc Conexa Meet webview</Text>
+      <WebView
+        style={styles.webview}
+        javaScriptEnabled
+        javaScriptCanOpenWindowsAutomatically
+        source={{
+          uri: url,
+        }}
+        onNavigationStateChange={console.log}
+        startInLoadingState
+        scalesPageToFit
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  webview: {
+    backgroundColor: "#fff",
+    flexGrow: 1,
+    width: 4000,
+    maxWidth: "100%",
   },
 });
