d7e8e51c32dfdc88e2df863a27096ffc7da112c6
Gabriel Rocha <gabriel.rocha@conexasaude.com.br>
2021-12-17 10:25:27 -0300

    with permissions

--- App.tsx
+++ App.tsx
@@ -1,23 +1,46 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { WebView } from "react-native-webview";
 import { url } from "./url";
+import { Camera } from "expo-camera";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (!hasPermission) {
+    return (
+      <View>
+        <Text>Grant Permission first</Text>
+      </View>
+    );
+  }
   return (
     <SafeAreaView style={styles.container}>
       <Text>Poc Conexa Meet webview</Text>
+
       <WebView
         style={styles.webview}
         javaScriptEnabled
-        javaScriptCanOpenWindowsAutomatically
         source={{
           uri: url,
         }}
-        onNavigationStateChange={console.log}
+        onNavigationStateChange={(e) => console.log(e.url.split("?")[0])}
         startInLoadingState
-        scalesPageToFit
+        allowsInlineMediaPlayback
+        allowsFullscreenVideo
+        domStorageEnabled
+        useWebKit
+        originWhitelist={["*"]}
+        mediaPlaybackRequiresUserAction={false}
       />
       <StatusBar style="auto" />
     </SafeAreaView>
@@ -33,8 +56,10 @@ const styles = StyleSheet.create({
   },
   webview: {
     backgroundColor: "#fff",
+    zIndex: 20,
     flexGrow: 1,
-    width: 4000,
+    width: 320,
+    height: 400,
     maxWidth: "100%",
   },
 });
